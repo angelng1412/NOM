50fcd26e1488694be770b6a2063672bbca24c1de
Levi <lolevsky@stuy.edu>
2017-02-13 22:50:10 -0500

    Added timing

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -22,6 +22,14 @@
 public class MergeSortTester 
 {
 
+    public static int[] makeArr(int num){
+	int[] retArr = new int[num];
+	for(int i = num; i > 0; i--){
+	    retArr[i - 1] = i;
+	}
+	return retArr;
+    }
+    
     /******************************
      * execution time analysis 
      * <INSERT YOUR DESCRIPTION HERE OF 
@@ -30,7 +38,34 @@ public class MergeSortTester
      ******************************/
     public static void main( String[] args ) 
     {
+	int[] arr0 = makeArr(1);
+	int[] arr1 = makeArr(10);
+	int[] arr2 = makeArr(100);
+	int[] arr3 = makeArr(99999);
+	long before, after;
+	
+       	System.out.println("\nSorting arr4-7...");
 
+	before = System.currentTimeMillis();
+	MergeSort.sort( arr0 );
+	after = System.currentTimeMillis();
+	System.out.println("Arr0: " + (after - before) + "\n");
+
+	before = System.currentTimeMillis();
+	MergeSort.sort( arr1 );
+	after = System.currentTimeMillis();
+	System.out.println("Arr1: " + (after - before) + "\n");
+
+	before = System.currentTimeMillis();
+	MergeSort.sort( arr2 );
+	after = System.currentTimeMillis();
+	System.out.println("Arr2: " + (after - before) + "\n");
+
+	before = System.currentTimeMillis();
+	MergeSort.sort( arr3 );
+	after = System.currentTimeMillis();
+	System.out.println("Arr3: " + (after - before) + "\n");
+	
     }//end main
 
 }//end class
