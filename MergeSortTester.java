e478eb2ea3360a13cbaaafff6d226581575b5477
EdwinMok <emok@stuy.edu>
2017-02-14 19:27:36 -0500

    Added comments for the runtime classification in MergeSortTester

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -1,7 +1,7 @@
-//Team NOM
-//
-//
-//
+// Team NOM - Angel Ng, Levi Olevsky, Edwin Mok
+// APCS2 pd5
+// HW#07 -- What Does the Data Say?
+// 2017-02-14
 
 /*======================================
   class MergeSortTester
@@ -14,14 +14,19 @@
 
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
-  n=1       time: 
-  n=10      time: 
-  n=100     time: 
-  ...
-  n=<huge>  time: 
+  n=1       time: 1
+  n=10      time: 1
+  n=100     time: 1
+  n=10^3    time: 1
+  n=10^4    time: 1
+  n=10^5    time: 172
+  n=10^6    time: 547
+  n=<huge>  time: 3810
 
   ANALYSIS:
-  <INSERT YOUR RESULTS ANALYSIS HERE>
+  As a smaller array is sorted using mergesort, the runtime is relatively small. When the items in the array reach 10,000, the process starts to take up longer runtime and grows really quickly.
+  Our hypothesis for the runtime is nlogn, and based on the experiments conducted, the data shows that it is somewhere in between linear and _________ runtime, which proves our hypothesis (correct/wrong) that the runtime classification for mergesort is (indeed nlogn/__________).
+  We also conclude that mergesort does not have a worst case scenario and a best case scenario, because it will always split the non-1-item arrays into 1-item arrays and then recursively merge them together, and the runtime depends only on the number of items in the given array.
   ======================================*/
 
 public class MergeSortTester 
